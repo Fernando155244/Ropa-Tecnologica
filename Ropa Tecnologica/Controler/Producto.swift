b17b546c1d728b55ecd6c9e43016118c0a1ac8fa
Fernando155244 <155244@udlondres.com>
2024-08-01 10:25:39 -0500

    “Comentario”

---
+++ Ropa Tecnologica/Controler/Producto.swift
@@ -0,0 +1,17 @@
+//
+//  Producto.swift
+//  Ropa Tecnologica
+//
+//  Created by user260925 on 8/1/24.
+//
+
+import Foundation
+
+struct Producto{
+    var image: String=""
+    var name: String=""
+    init(image: String, name: String) {
+        self.image = image
+        self.name = name
+    }
+}
---
+++ Ropa Tecnologica/Controler/ProductosControler.swift
@@ -0,0 +1,15 @@
+//
+//  ProductosControler.swift
+//  Ropa Tecnologica
+//
+//  Created by user260925 on 8/1/24.
+//
+
+import Foundation
+import UIKit
+
+class ProductoCollectionViewCell: UICollectionView
+{
+    @IBOutlet weak var prodcutoImagen: UIImageView!
+    @IBOutlet weak var ProdcutoLabel: UILabel!
+}
